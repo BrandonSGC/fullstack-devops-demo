e40c0072d6c38142f606df1b3258ac293d07480e
BrandonSGC <brangmz20@gmail.com>
2025-09-07 16:00:26 -0600

    feat(infra): creating needed infrastructure

--- infra/providers.tf
+++ infra/providers.tf
@@ -9,5 +9,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "16e28336-0da6-4238-bf6c-6f3ee682a721"
+  subscription_id = var.subscription_id
 }
---
+++ infra/variables.tf
@@ -0,0 +1,33 @@
+variable "subscription_id" {
+  description = "The Azure Subscription ID for the providers.tf file"
+  type        = string
+}
+
+variable "rg_name" {
+  description = "The name of the Resource Group"
+  type        = string
+  default     = "rg-fullstack-devops-demo"
+}
+
+variable "location" {
+  description = "The Azure region to deploy resources in"
+  type        = string
+  default     = "Canada Central"
+}
+
+variable "db_name" {
+  description = "The name of the database"
+  type        = string
+  default     = "fullstackdb"
+}
+
+variable "db_admin" {
+  description = "The database administrator username"
+  type        = string
+  default     = "psqladmin"
+}
+variable "db_password" {
+  description = "The database administrator password"
+  type        = string
+  default     = "P@ssw0rd!"
+}

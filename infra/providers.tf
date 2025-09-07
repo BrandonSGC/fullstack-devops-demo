ddd934dfc0ffb830d138d6c069d871acff385991
BrandonSGC <brangmz20@gmail.com>
2025-09-07 14:44:25 -0600

    chore(infra): configuring azure provider

---
+++ infra/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "16e28336-0da6-4238-bf6c-6f3ee682a721"
+}

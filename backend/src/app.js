8585ee3babdd41f2e4b8b002e80b7ef2ce9171f8
Brandon Gómez <125517255+BrandonSGC@users.noreply.github.com>
2025-09-06 23:11:35 -0600

    fix(backend): adding CORS to allow frontend requests (#3)
    I installed the CORS packet in order to allow requests from the React frontend app to the API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import userRoutes from "./routes/user.routes.js";
+import cors from "cors";
 
 const app = express();
 
+// Middlewares
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {

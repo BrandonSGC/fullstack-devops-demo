00b1b5853251ac3d1e061a633d546ae9f7766c0c
BrandonSGC <brangmz20@gmail.com>
2025-09-06 20:29:13 -0600

    feat(backend): adding create user and get by email endpoints

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 
@@ -7,5 +8,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
 });
+
+app.use("/api/users", userRoutes);
 
 export default app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,16 @@
+import dotenv from "dotenv";
 import app from "./app.js";
+import { sequelize } from "./db/connection.js";
 
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 async function main() {
   try {
+    // Handle DB
+    await sequelize.sync(/*{force: true}*/);
+
+    // Server
     app.listen(PORT);
     console.log(`Server running at http://localhost:${PORT}`);
   } catch (error) {
